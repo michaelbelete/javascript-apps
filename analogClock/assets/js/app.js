27d891525da8f09c064d11f7509c0c6e61ab1c13
michael belete <it.michael.belete@gmail.com>
2021-02-14 00:02:06 +0300

    finished analog clock js

--- analogClock/assets/js/app.js
+++ analogClock/assets/js/app.js
@@ -1,3 +1,27 @@
 const hour = document.querySelector('#hour');
 const minute = document.querySelector('#minute');
 const second = document.querySelector('#second');
+
+function getDate() {
+    const dateNow = new Date()
+
+    const nowHour = dateNow.getHours()
+
+    const nowMinute = dateNow.getMinutes()
+
+    const nowSecond = dateNow.getSeconds()
+
+    const hourDegrees = ((nowHour / 12) * 360) + ((nowMinute / 60) * 30) + 90;
+    const minuteDegrees = ((nowMinute / 60) * 360) + ((nowSecond / 60) * 6) + 90;
+    const secondDegrees = ((nowSecond / 60) * 360) + 90;
+
+
+    hour.style.transform = `rotate(${hourDegrees}deg)`;
+    minute.style.transform = `rotate(${minuteDegrees}deg)`;
+    second.style.transform = `rotate(${secondDegrees}deg)`;
+
+}
+
+setInterval(getDate, 1000);
+
+getDate();
